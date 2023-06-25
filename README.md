ff6e4632d1923f9e9768c9f03a5caab88e947e94
henry <hmarichal93@gmail.com>
2023-06-25 16:21:41 -0300

    scripts

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# uruDendro
+# UruDendro, a public dataset of cross-section images of Pinus Taeda 
+Link to web-site: https://iie.fing.edu.uy/proyectos/madera/
+
+## Download Dataset
+Download the dataset using the following command:
+```bash
+python scripts/dataset.py --download --dataset_path DATASET_PATH
+```
+where DATASET_PATH is the path where the dataset will be downloaded. DATASET_PATH must be absolute path.
+
+## Visualize Dataset
+Visualize the dataset using the following command:
+```bash
+python scripts/dataset.py --visualize --annotation_file ANNOTATION_FILE
+```
+where ANNOTATION_FILE is the path to the annotation file.
+
+## Metric Evaluation
+Tree ring Detection evaluation metrics used by UruDendro. The evaluation code provided here can be used to obtain results on the publicly available UruDendro dataset. It computes multiple metrics described below.
+```bash
+python scripts/metric_influence_area.py --dt_filename DETECTION_FILENAME
+ --gt_filename GROUND_TRUTH_FILENAME  --img_FILENAME IMAGE_FILENAME 
+ --cx CX --cy CY --th THRESHOLD --output_dir OUTPUT_DIR
+```
+where DETECTION_FILENAME is the path to the detection file, GROUND_TRUTH_FILENAME is the path to the ground truth file,
+IMAGE_FILENAME is the path to the image file, CX is the x pith coordinate in pixels, CY is the y pith coordinate in pixel, 
+THRESHOLD is the threshold to consider a detection as valid (between 0 and 1) and OUTPUT_DIR is the path to the 
+results output directory.
---
+++ scripts/dataset.py
@@ -0,0 +1,101 @@
+"""Script for downloading and preprocessing the dataset."""
+import os
+import sys
+import argparse
+from pathlib import Path
+import json
+
+def download_file_and_store_id_in_dir(images_url, dataset_path, local_filename="images.zip"):
+
+    os.system(f"cd {dataset_path} && wget -r -np -nH --cut-dirs=3 -R index.html {images_url}")
+    page_name = images_url.split("/")[-2]
+    #1.1 move file to images.zip
+    os.system(f"mv {dataset_path}/{page_name}/index.html.tmp {dataset_path}/{local_filename}")
+    #1.2 remove 244 directory
+    os.system(f"rm -r {dataset_path}/{page_name}")
+    return
+
+
+def load_json(filepath: str) -> dict:
+    """
+    Load json utility.
+    :param filepath: file to json file
+    :return: the loaded json as a dictionary
+    """
+    with open(str(filepath), 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def write_json(dict_to_save: dict, filepath: str) -> None:
+    """
+    Write dictionary to disk
+    :param dict_to_save: serializable dictionary to save
+    :param filepath: path where to save
+    :return: void
+    """
+    with open(str(filepath), 'w') as f:
+        json.dump(dict_to_save, f)
+
+def process_annotations(annotations_path, images_path):
+    annotations_path = Path(f"{annotations_path}/")
+    for file in annotations_path.rglob("*"):
+        file_json = load_json(file)
+        #file_json["imageData"] = None
+        img_name = file.name.split(".")[0].split("-")[0]
+        file_json["imagePath"] = str(f"{images_path}/{img_name}.png")
+        write_json(file_json, file)
+
+
+def unzip_file(path, filename):
+    os.system(f"mkdir -p {path} && cd {path} && unzip -o -q ../{filename} && rm ../{filename}")
+    return
+
+def download_dataset(dataset_path="/home/henry/Documents/repo/fing/uruDendro/data2"):
+    "Download the dataset from url"
+    # create dataset path directory if not exists
+    os.system(f"mkdir -p {dataset_path}")
+    #1.0 download images from url
+    images_url = "https://iie.fing.edu.uy/proyectos/madera/download/244/"
+    images_name = "images.zip"
+    images_path = f"{dataset_path}/images"
+    download_file_and_store_id_in_dir(images_url, dataset_path, local_filename=images_name)
+    #2.0 unzip images
+    unzip_file(images_path, images_name)
+
+    #3.0 download labels from url
+    annotations_url = "https://iie.fing.edu.uy/proyectos/madera/download/246/"
+    annotations_name = "labels.zip"
+    download_file_and_store_id_in_dir(annotations_url, dataset_path, local_filename=annotations_name)
+    annotations_path = f"{dataset_path}/annotations"
+    unzip_file(annotations_path, annotations_name)
+
+    #4.0 download pith annotations
+    pith_url = "https://iie.fing.edu.uy/proyectos/madera/download/248/"
+    pith_name = "pith.csv"
+    pith_path = f"{dataset_path}/pith"
+    download_file_and_store_id_in_dir(pith_url, dataset_path, local_filename=pith_name)
+    #os.system(f"rmdir {pith_path}")
+    #unzip_file(pith_path, pith_name)
+
+    #5.0 process annotations
+    process_annotations(annotations_path, images_path)
+    return
+def visualize_annotation(annotation_file):
+    os.system(f"labelme {annotation_file}")
+    return
+
+
+if __name__ == "__main__":
+    #create parser. Two options. Option 1: download dataset. Option 2: visualize annotation
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--download', action='store_true', help='download dataset')
+    parser.add_argument('--dataset_path', type=str, help='path to download dataset')
+    parser.add_argument('--visualize', action='store_true', help='visualize annotation')
+    parser.add_argument('--annotation_file', type=str, help='annotation file to visualize')
+    args = parser.parse_args()
+    if args.download:
+        download_dataset(args.dataset_path)
+
+    if args.visualize:
+        visualize_annotation(args.annotation_file)
